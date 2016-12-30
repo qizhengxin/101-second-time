cf0b97dd1381bc155990c894e2d6997efcbf5452
qizhengxin <qizhengxin721@gmail.com>
2016-12-30 17:16:23 +0800

    member

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :groups
-  has_many :articles     
+  has_many :articles
+  has_many :group_relationships
+  has_many :paticipated_groups, through: :group_relationships, source: :group
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230052626) do
+ActiveRecord::Schema.define(version: 20161230075734) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "content"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "group_relationships", force: :cascade do |t|
     t.integer  "group_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
