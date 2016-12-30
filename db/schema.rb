272da6bfe1b8d112095074d2868a2de8f1d71923
qizhengxin <qizhengxin721@gmail.com>
2016-12-30 13:31:36 +0800

    relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230043000) do
+ActiveRecord::Schema.define(version: 20161230052626) do
+
+  create_table "articles", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
