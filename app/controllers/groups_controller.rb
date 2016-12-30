a88abef6f75435a96f63a36e49c19508d3d6602f
qizhengxin <qizhengxin721@gmail.com>
2016-12-30 13:02:05 +0800

    permmion

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 
   def index
@@ -26,10 +26,17 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "你不具备权限，哈哈"
+    end
   end
 
   def update
     @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "你不具备权限，哈哈"
+    end
+
     if @group.update(group_params)
       redirect_to groups_path, notice: "更新成功"
     else
@@ -39,6 +46,9 @@ class GroupsController < ApplicationController
 
   def destroy
     @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "你不具备权限，哈哈" 
+    end
     @group.destroy
     flash[:alert] = "删除成功"
     redirect_to groups_path
