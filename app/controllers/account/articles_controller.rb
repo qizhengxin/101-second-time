449e416ca35b993bfdbfeff6915d800a04d83144
qizhengxin <qizhengxin721@gmail.com>
2017-01-01 17:44:27 +0800

    success

--- app/controllers/account/articles_controller.rb
+++ app/controllers/account/articles_controller.rb
@@ -5,19 +5,30 @@ class Account::ArticlesController < ApplicationController
     @articles = current_user.articles
   end
 
+
+
   def edit
     @article = current_user.articles.find(params[:id])
   end
 
   def update
     @article = current_user.articles.find(params[:id])
-    @article.update
-    redirect_to account_article_path
+    @article.update(article_params)
+    redirect_to account_articles_path
   end
+
+
 
   def destroy
     @article = current_user.articles.find(params[:id])
     @article.destroy
-    redirect_to account_article_path
+    redirect_to account_articles_path
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:content)
   end
 end
